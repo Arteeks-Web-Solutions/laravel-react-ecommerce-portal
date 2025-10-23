9c7bcfaf71b3059357807063c614a040a79c1e09
UndercoverNL <mail@undercovernl.com>
2025-10-23 12:36:18 +0200

    feat(product): add soft delete functionality to product model and migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,12 @@ namespace TechStore\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
